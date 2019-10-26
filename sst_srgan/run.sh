86dc00a5f3922f616d49de63966f41b0f00bc2fd
Yixin Lian <yl4089@holmes.cm.cluster>
2019-10-26 16:47:37 -0400

    update run.sh

--- sst_srgan/run.sh
+++ sst_srgan/run.sh
@@ -9,17 +9,18 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH -c 1                     # The number of cpu cores to use.
 #SBATCH --time=12:00:00              # The time the job will take to run (here, 1 min)
-#SBATCH --mem-per-cpu=32gb        # The memory the job will use per cpu core.
+#SBATCH --mem-per-cpu=64gb        # The memory the job will use per cpu core.
 #SBATCH --constraint=p100
 
 module load cuda10.0/toolkit
 module load cuda10.0/blas
 module load cudnn/cuda_10.0_v7.6.2
 
-cd /rigel/ocp/users/zw2533/sst_superresolution/sst_srgan
+cd /rigel/ocp/users/yl4089/sst_new/sst_superresolution/sst_srgan
 
-source activate sst
+#source activate sst
 
-python runner.py --job_name=ocean_4x --dataset_name=hr_lr_2timesteps --epochs=20 --batch_size=4
+python runner.py --job_name=ocean_4x --epochs=20
+#python runner.py --job_name=ocean_4x --dataset_name=hr_lr_2timesteps --epochs=20 --batch_size=4
 
 # End of script
